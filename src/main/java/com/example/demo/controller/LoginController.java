2083852ebc6c564329011563b10fe42c0863b6e1
pejak4 <53259486+pejak4@users.noreply.github.com>
2019-11-26 15:33:22 +0100

    Modify login controller
    Changing login method, adding new changePassword method

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -5,8 +5,10 @@ import com.example.demo.exception.ResourceConflictException;
 import com.example.demo.model.User;
 import com.example.demo.security.TokenUtils;
 import com.example.demo.service.*;
+import com.example.demo.view.UserViewChangePassword;
 import com.example.demo.view.UserViewLogin;
 import com.example.demo.view.UserViewRegister;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,10 +19,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
@@ -42,10 +42,15 @@ public class LoginController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/login")
-    public ResponseEntity<UserTokenState> login(@RequestBody UserViewLogin user) throws AuthenticationException, IOException {
+    public ResponseEntity<UserTokenState> login(@RequestBody UserViewLogin user) throws AuthenticationException, NotFoundException {
+
+        User logged = this.userService.findOneByEmailAndPassword(user.getEmail(), user.getPassword());
+
+        if (logged == null)
+            throw new NotFoundException("Not existing user");
 
         final Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword(), logged.getAuthorities()));
 
         //ubaci username(email) + password u kontext
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -55,9 +60,10 @@ public class LoginController {
         String jwt = tokenUtils.generateToken(userToken.getEmail(), userToken.getRole().getRole());
         int expiresIn = tokenUtils.getExpiredIn();
         //vrati token kao odgovor na uspesnu autentifikaciju
-        return ResponseEntity.ok(new UserTokenState(jwt, expiresIn));
+        return ResponseEntity.ok(new UserTokenState(jwt, (long) expiresIn, userToken.isFirstTimeLogged()));
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping(value = "/register")
     public ResponseEntity<?> register(@RequestBody UserViewRegister user, UriComponentsBuilder ucBuilder) {
 
@@ -73,5 +79,19 @@ public class LoginController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(ucBuilder.path("/api/user/userId}").buildAndExpand(saveUser.getId()).toUri());
         return new ResponseEntity<User>(saveUser, HttpStatus.CREATED);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/changePassword")
+    public ResponseEntity<?> changePassword(@RequestBody UserViewChangePassword user) {
+
+        if (!user.getNewPass().equals(user.getNewRepeatPass()))
+            return null;
+
+        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        loggedUser.setPassword(user.getNewPass());
+        loggedUser.setFirstTimeLogged(false);
+
+        return new ResponseEntity<User>(loggedUser, HttpStatus.OK);
     }
 }
