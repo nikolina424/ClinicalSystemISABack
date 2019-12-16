d31b7354dbd8beaa8ffdd5ea2134f066621333ac
nikolina424 <57722902+nikolina424@users.noreply.github.com>
2019-12-16 20:59:35 +0100

    Created service for clinic

---
+++ src/main/java/com/example/demo/service/ClinicService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Clinic;
+import com.example.demo.repository.ClinicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClinicService {
+
+    @Autowired
+    private ClinicRepository clinicRepository;
+
+    public List<Clinic> getClinics(){
+        return this.clinicRepository.findAll();
+    }
+
+}
