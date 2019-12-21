022363b56c4c7c1d6a8257ca1727cb7818da481c
pejak4 <53259486+pejak4@users.noreply.github.com>
2019-12-21 21:04:52 +0100

    Add method in UserController for upload images

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.UserEditDTO;
 import com.example.demo.model.User;
 import com.example.demo.model.UserRole;
+import com.example.demo.service.StorageService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 import static com.example.demo.model.UserRole.DOCTOR;
@@ -20,6 +23,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private StorageService storageService;
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/getPatients")
@@ -63,6 +69,24 @@ public class UserController {
             loggedUser.setCity(userEdit.getCity());
             loggedUser.setCountry(userEdit.getCountry());
             return new ResponseEntity<>(this.userService.save(loggedUser), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping(path = "/addImageToUser")
+    public ResponseEntity<?> addImageToUser(@RequestParam("file") MultipartFile file) {
+
+        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (loggedUser != null) {
+            try {
+                String imageName = this.storageService.store(file);
+                loggedUser.setImage(imageName);
+                return new ResponseEntity<>(this.userService.save(loggedUser), HttpStatus.OK);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Fail to upload image!");
+            }
         }
 
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
