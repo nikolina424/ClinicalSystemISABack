02832854eec3b4977ea4c54c031b9cbac2840dd6
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-02-04 16:22:37 +0100

    Modify data postres sql
    Add 3 new clinic admin for each clinic
    Add new variables to the clinic table

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,16 +1,19 @@
-insert into clinic_center (name) values ('Klinicki Centar NS');
-
-insert into clinic (name, clinic_center_id) values ('Klinika 1', 1);
-insert into clinic (name, clinic_center_id) values ('Klinika 2', 1);
-insert into clinic (name, clinic_center_id) values ('Klinika 3', 1);
-
-insert into medical_record (id) values (1);
-insert into medical_record (id) values (2);
-
 insert into users (first_name, last_name, email, password, address, city, country, phone_number, user_id, role, enabled, first_time_logged, predefined) values ('Stefan', 'Pejakovic', 'spejakovic5@gmail.com', '$2a$10$LUg80LWtePrHM/Oba6pJHu0BKXjgCA4GcbeTB6TD4Yos3APqstUCy', 'XVI Divizija 137', 'Kula', 'Srbija', 0613729356, 0208997, 'ADMINCC', true, true, true);
+insert into users (first_name, last_name, email, password, address, city, country, phone_number, user_id, role, enabled, first_time_logged, predefined) values ('Admin1', 'Admin1', 'admin1@gmail.com', '$2a$10$LUg80LWtePrHM/Oba6pJHu0BKXjgCA4GcbeTB6TD4Yos3APqstUCy', 'Radnicka 22', 'Kula', 'Srbija', 0649834349, 493481394, 'ADMINC', true, true, true);
+insert into users (first_name, last_name, email, password, address, city, country, phone_number, user_id, role, enabled, first_time_logged, predefined) values ('Admin2', 'Admin2', 'admin2@gmail.com', '$2a$10$LUg80LWtePrHM/Oba6pJHu0BKXjgCA4GcbeTB6TD4Yos3APqstUCy', 'Novaka Pejicica 145', 'Kula', 'Srbija', 0619800430, 43123232, 'ADMINC', true, true, true);
+insert into users (first_name, last_name, email, password, address, city, country, phone_number, user_id, role, enabled, first_time_logged, predefined) values ('Admin3', 'Admin3', 'admin3@gmail.com', '$2a$10$LUg80LWtePrHM/Oba6pJHu0BKXjgCA4GcbeTB6TD4Yos3APqstUCy', 'Zarka Zrenjanina 10a', 'Kula', 'Srbija', 063434556, 451334313, 'ADMINC', true, true, true);
 insert into users (first_name, last_name, email, password, address, city, country, phone_number, user_id, role, enabled, first_time_logged, predefined) values ('Nikolina', 'Ivankovic', 'inina007@gmail.com', '$2a$10$LUg80LWtePrHM/Oba6pJHu0BKXjgCA4GcbeTB6TD4Yos3APqstUCy', 'Marsala Tita 6', 'Kula', 'Srbija', 0659438132, 1505997, 'DOCTOR', true, true, true);
 insert into users (first_name, last_name, email, password, address, city, country, phone_number, user_id, role, enabled, first_time_logged, predefined) values ('Marko', 'Markic', 'mmic@gmail.com', '$2a$10$LUg80LWtePrHM/Oba6pJHu0BKXjgCA4GcbeTB6TD4Yos3APqstUCy', 'Kisacka 41', 'Novi Sad', 'Srbija', 0634850341, 9834130, 'PATIENT', true, true, true);
 insert into users (first_name, last_name, email, password, address, city, country, phone_number, user_id, role, enabled, first_time_logged, predefined) values ('Nikola', 'Narkic', 'nark@gmail.com', '$2a$10$LUg80LWtePrHM/Oba6pJHu0BKXjgCA4GcbeTB6TD4Yos3APqstUCy', 'Krusevacka 3', 'Krusevac', 'Srbija', 0624831490, 5503491, 'PATIENT', true, true, true);
+
+insert into clinic_center (name, admin_id) values ('Klinicki Centar NS', 1);
+
+insert into clinic (name, description, address, clinic_center_id, admin_id) values ('Klinika 1', 'Opsta bolnica', 'Cara Dusana 55', 1, 2);
+insert into clinic (name, description, address, clinic_center_id, admin_id) values ('Klinika 2', 'Infektivna klinika', 'Zvezdara 13a', 1, 3);
+insert into clinic (name, description, address, clinic_center_id, admin_id) values ('Klinika 3', 'Hirurska klinika', 'Kramerova 2', 1, 4);
+
+insert into medical_record (id) values (1);
+insert into medical_record (id) values (2);
 
 insert into operation (description, date_time, duration_hours) values ('Transplatacija bubrega', '2019-9-8', 7);
 insert into operation (description, date_time, duration_hours) values ('Transplatacija srca', '2019-3-25', 12);
@@ -52,7 +55,7 @@ insert into role_authorities (role_name, authority_name) values ('ADMINCC', 'MAN
 insert into role_authorities (role_name, authority_name) values ('ADMINCC', 'MANAGE_CLINIC');
 insert into role_authorities (role_name, authority_name) values ('DOCTOR', 'MANAGE_OPERATION');
 
-alter sequence users_id_seq restart with 5;
+alter sequence users_id_seq restart with 8;
 alter sequence clinic_center_id_seq restart with 2;
 alter sequence clinic_id_seq restart with 4;
 alter sequence operation_id_seq restart with 3;
