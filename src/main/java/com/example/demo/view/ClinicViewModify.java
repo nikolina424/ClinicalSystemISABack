58bf91815921fab47ae016047671a734ead0e2a1
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-02-04 16:23:03 +0100

    New view class
    When information is send to modify clinic

---
+++ src/main/java/com/example/demo/view/ClinicViewModify.java
@@ -0,0 +1,18 @@
+package com.example.demo.view;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClinicViewModify {
+
+    private Long clinicId;
+    private String name;
+    private String description;
+    private String address;
+}
