ae4df515fe819892f16fabd660cdad4f07360df6
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-02-04 16:23:20 +0100

    New clinic jpa repository

--- src/main/java/com/example/demo/repository/ClinicRepository.java
+++ src/main/java/com/example/demo/repository/ClinicRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ClinicRepository extends JpaRepository<Clinic, Long> {
 
     Clinic findOneByName(String name);
+    Clinic findOneByAdminId(Long id);
+    Clinic findOneById(Long id);
 }
