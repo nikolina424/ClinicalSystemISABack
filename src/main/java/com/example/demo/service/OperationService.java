fd5d79c8fc9ddee954bbe4f01eaf6571b6321723
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-02-05 23:56:35 +0100

    New method in OperationService class
    Modify old save operation method with new fields
    Add new save operation method when doctor is scheduling operation

--- src/main/java/com/example/demo/service/OperationService.java
+++ src/main/java/com/example/demo/service/OperationService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Operation;
+import com.example.demo.model.User;
 import com.example.demo.repository.OperationRepository;
 import com.example.demo.view.OperationViewSchedule;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,14 @@ public class OperationService {
 
     public Operation save(OperationViewSchedule op) {
         Operation operation = Operation.builder().description(op.getDescription()).duration(op.getDuration())
-                                .dateTime(op.getDateTime()).build();
+                                .dateTime(op.getDateTime()).doctor(op.getDoctor()).price(op.getPrice()).build();
+
+        return this.operationRepository.save(operation);
+    }
+
+    public Operation saveDoctor(OperationViewSchedule op, User doctor) {
+        Operation operation = Operation.builder().description(op.getDescription()).duration(op.getDuration())
+                .dateTime(op.getDateTime()).doctor(doctor).price(op.getPrice()).build();
 
         return this.operationRepository.save(operation);
     }
