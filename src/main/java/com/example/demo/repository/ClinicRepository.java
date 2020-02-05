1c7d641246a8402de60ff51cf509a61604cae0d1
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-02-05 23:57:33 +0100

    Add imports

--- src/main/java/com/example/demo/repository/ClinicRepository.java
+++ src/main/java/com/example/demo/repository/ClinicRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Clinic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ClinicRepository extends JpaRepository<Clinic, Long> {
 
