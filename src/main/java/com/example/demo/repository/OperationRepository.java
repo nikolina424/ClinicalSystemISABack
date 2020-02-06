c8f54d8cb1b8d2c4f7906016b086fda9c2970b98
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-02-06 20:10:30 +0100

    New methods for modify, delete, search and add operation

--- src/main/java/com/example/demo/repository/OperationRepository.java
+++ src/main/java/com/example/demo/repository/OperationRepository.java
@@ -2,6 +2,27 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface OperationRepository extends JpaRepository<Operation, Long> {
+
+    Operation findOneById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "delete from operation op where op.id=:id")
+    void deleteOperation(@Param("id") Long id);
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "delete from operation_room opr where opr.operation_id=:id")
+    void deleteOperationRoom(@Param("id") Long id);
+
+    @Query(nativeQuery = true, value = "select op.id, op.date_time, op.description, op.duration_hours, op.price, op.doctor_id from operation_room opr left join operation op on opr.operation_id=op.id where opr.room_id in (select r.id from room r left join clinic cl on r.clinic_id = cl.id where cl.id = (select cl.id from clinic cl left join users us on cl.admin_id=us.id where us.id=:id) and r.reserved=true)")
+    List<Operation> findAllOperationsOfClinic(@Param("id") Long id);
 }
--- src/main/java/com/example/demo/service/OperationService.java
+++ src/main/java/com/example/demo/service/OperationService.java
@@ -7,11 +7,25 @@ import com.example.demo.view.OperationViewSchedule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class OperationService {
 
     @Autowired
     private OperationRepository operationRepository;
+
+    public List<Operation> findAll() {
+        return this.operationRepository.findAll();
+    }
+
+    public Operation findOneById(Long id) {
+        return this.operationRepository.findOneById(id);
+    }
+
+    public Operation save(Operation op) {
+        return this.operationRepository.save(op);
+    }
 
     public Operation save(OperationViewSchedule op) {
         Operation operation = Operation.builder().description(op.getDescription()).duration(op.getDuration())
@@ -25,5 +39,17 @@ public class OperationService {
                 .dateTime(op.getDateTime()).doctor(doctor).price(op.getPrice()).build();
 
         return this.operationRepository.save(operation);
+    }
+
+    public void deleteOperation(Long id) {
+        this.operationRepository.deleteOperation(id);
+    }
+
+    public void deleteOperationRoom(Long id) {
+        this.operationRepository.deleteOperationRoom(id);
+    }
+
+    public List<Operation> findOperationsOfClinic(Long id) {
+        return this.operationRepository.findAllOperationsOfClinic(id);
     }
 }
